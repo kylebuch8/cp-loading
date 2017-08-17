87588d2ec8af31751089f7239c18d225b6084b44
Kyle Buchanan <kylebuch8@gmail.com>
2017-08-17 16:27:17 -0400

    build changes and version bump

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ const HtmlSplitter = require('polymer-build').HtmlSplitter;
 const babel = require('gulp-babel');
 const htmlMinifier = require('gulp-html-minifier');
 const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const replace = require('gulp-replace');
 const sourcesHtmlSplitter = new HtmlSplitter();
 const del = require('del');
 
 gulp.task('clean', () => {
-  return del(['dist/**/*'])
+  return del(['./*.compiled.*'])
 });
 
 gulp.task('compile', () => {
@@ -20,15 +22,14 @@ gulp.task('compile', () => {
       collapseWhitespace: true,
       minifyCSS: true
     })))
+    .pipe(gulpif(/\.html$/, replace(/<link rel="import" href="(.*?)(.html)">/g, '<link rel="import" href="$1.compiled$2">')))
     .pipe(sourcesHtmlSplitter.rejoin())
-    .pipe(gulp.dest('dist/compiled/'));
-});
-
-gulp.task('build', () => {
-  return gulp.src(['./*.html', './*.js', '!./gulpfile.js'])
-    .pipe(gulp.dest('dist/default/'));
+    .pipe(rename({
+      suffix: ".compiled"
+    }))
+    .pipe(gulp.dest('./'));
 });
 
 gulp.task('default',
-  gulp.series('clean', gulp.parallel('build', 'compile'))
+  gulp.series('clean', 'compile')
 );
